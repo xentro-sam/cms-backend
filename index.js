7245c9cf9065250132172a3b4ab19770a3b715e1
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-09 22:46:03 +0530

    feat: specify cors

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 const cmsRoutes = require('./src/routers/cms.routes');
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,PUT,POST,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
