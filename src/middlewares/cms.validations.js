6643f2f26b84e9ccd8eec6bb4eaf1db186c0ff1b
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-10 14:26:55 +0530

    fix: fix joi schema validations

--- src/middlewares/cms.validations.js
+++ src/middlewares/cms.validations.js
@@ -2,15 +2,19 @@ const {idValidation} = require('../schemas/cms.schemas');
 
 const validateId = (req, res, next) => {
   const {id, entryId} = req.params;
-  try {
-    idValidation.validate({id});
-    if (entryId) {
-      idValidation.validate({id: entryId});
-    }
-  } catch (error) {
+  const {error} = idValidation.validate({id});
+  if (error) {
     res.status(400);
     res.json({message: error.message});
     return;
+  }
+  if (entryId) {
+    const {error} = idValidation.validate({id: entryId});
+    if (error) {
+      res.status(400);
+      res.json({message: error.message});
+      return;
+    }
   }
   next();
 };
