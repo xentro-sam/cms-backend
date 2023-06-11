4dae26a37665408d5a4f91809d27d7e99dbc28f2
Shivam Agarwal <itisshivam22@gmail.com>
2023-06-12 02:24:26 +0530

    feat: create database if it already does not exists

--- db/models/index.js
+++ db/models/index.js
@@ -16,34 +16,77 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-    .readdirSync(__dirname)
-    .filter((file) => {
-      return (
-        file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-      );
-    })
-    .forEach((file) => {
-      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-      db[model.name] = model;
-    });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-if (process.env.NODE_ENV !== 'test') {
-  sequelize.sync({force: true}).then(() => {
-    console.log('Database is synced');
+// Add this code to automatically create the database if it doesn't exist
+if (config.dialect === 'postgres') {
+  // Create a temporary Sequelize instance without specifying a database name
+  const tempSequelize = new Sequelize(config.username, config.password, '', {
+    host: config.host,
+    dialect: config.dialect,
+    port: config.port,
+    logging: false,
   });
+
+  // Use the temporary Sequelize connection to check if the database exists
+  tempSequelize
+      .query(`SELECT 1 FROM pg_database WHERE datname = '${config.database}';`)
+      .then(([result]) => {
+        if (result.length === 0) {
+        // The database doesn't exist, so we can create it
+          return tempSequelize.query(`CREATE DATABASE "${config.database}";`);
+        }
+        // The database already exists, no need to create it
+        return Promise.resolve();
+      })
+      .then(() => {
+        console.log(`Database "${config.database}" created or already exists`);
+        // Now that the database exists, initialize Sequelize with the full configuration
+        sequelize = new Sequelize(config.database, config.username, config.password, config);
+        initModels();
+        runMigrations();
+      })
+      .catch((error) => {
+        console.error('Error creating or checking database:', error);
+      });
+} else {
+  initModels();
+  runMigrations();
 }
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+function initModels() {
+  fs.readdirSync(__dirname)
+      .filter((file) => {
+        return (
+          file.indexOf('.') !== 0 &&
+        file !== basename &&
+        file.slice(-3) === '.js' &&
+        file.indexOf('.test.js') === -1
+        );
+      })
+      .forEach((file) => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+      });
+
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+}
+
+function runMigrations() {
+  const {exec} = require('child_process');
+
+  exec('npx sequelize db:migrate', (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error running migrations:', error);
+      return;
+    }
+    console.log('Migrations executed successfully');
+  });
+}
 
 module.exports = db;
